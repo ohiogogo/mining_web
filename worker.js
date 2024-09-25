9ad023742bb84496fccf57d275f09b3112171ee0
david luo <mudsun@gmail.com>
2024-09-25 09:49:55 +0800

    asmCrypto.js not working, switch to WebCrypto instead

--- worker.js
+++ worker.js
@@ -1,23 +1,22 @@
-// 引入 asmCrypto
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/asmCrypto.js/2.3.2/asmcrypto.all.min.js');
-
-onmessage = function(e) {
+onmessage = async function(e) {
     const { inputText, difficulty, startR, rangeSize, target } = e.data;
     let r = startR;
     const endR = startR + rangeSize;
     let hashCount = 0;
 
     while (r < endR) {
-        // 使用 asmCrypto 进行 SHA-256 计算
-        const hash = asmCrypto.SHA256.hex(inputText + r);
+        const data = new TextEncoder().encode(inputText + r);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', data); // 使用 WebCrypto 进行哈希计算
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
         hashCount++;
-        const hashBigInt = BigInt("0x" + hash);
+        const hashBigInt = BigInt("0x" + hashHex);
 
         if (hashBigInt < target) {
             postMessage({
                 found: true,
                 r: r,
-                hash: hash,
+                hash: hashHex,
                 hashCount: hashCount
             });
             break;
